de229fa7d37d9924d14e71678237b5d4b883f601
Fuffi <ufinii@gmail.com>
2014-04-30 14:54:18 +0100

    v0.0.4, optional properties added to embed

--- app/helpers/google_data_layer_helper.rb
+++ app/helpers/google_data_layer_helper.rb
@@ -11,14 +11,15 @@ module GoogleDataLayerHelper
     end
   end
 
-  def google_data_layer_embed_code
+  def google_data_layer_embed_code(additional_properties = "")
     %Q{
       <!-- Google Data Layer -->
       <script type="text/javascript">
         var dataLayer = [{
           'ga_id': '#{GoogleDataLayer::Rails::Config.google_id}',
           'page_pageName': '#{@google_data_layer_page_name.to_s}',
-          'page_pageType': '#{@google_data_layer_page_type.to_s}'
+          'page_pageType': '#{@google_data_layer_page_type.to_s}',
+          #{additional_properties}
         }];
 
         var dataLayerPushVirtualPageViewEvent = function (pageName) {
--- lib/google_data_layer/rails/version.rb
+++ lib/google_data_layer/rails/version.rb
@@ -1,5 +1,5 @@
 module GoogleDataLayer
   module Rails
-    VERSION = "0.0.3"
+    VERSION = "0.0.4"
   end
 end

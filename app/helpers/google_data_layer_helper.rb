0b16773db71e9ec26ca1ffbaa0e6067a3b7b52d6
David Santoro <soulnafein@gmail.com>
2014-05-29 14:00:40 +0000

    Fixing error

--- app/helpers/google_data_layer_helper.rb
+++ app/helpers/google_data_layer_helper.rb
@@ -15,7 +15,8 @@ module GoogleDataLayerHelper
     dataLayer = [
       {
         ga_id: GoogleDataLayer::Rails::Config.google_id,
-        page_pageName: @google_data_layer_embed_code.to_s
+        page_pageName: @google_data_layer_page_name.to_s,
+        page_pageType: @google_data_layer_page_type.to_s
       }.merge(additional_parameters)
     ]
     %Q{
